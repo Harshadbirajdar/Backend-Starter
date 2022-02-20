0ff5d5f6d87609aade65a78d518d04b7de574e14
harshadbirajdar <hbirajdar1@gmail.com>
2022-02-20 21:57:40 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const { errorHandler, notFound } = require('./middleware/errorMiddleware')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("tiny"));
+
+// import all routes here
+const user = require("./route/user");
+
+// router middleware
+app.use("/api/v1", user);
+
+app.use(notFound)
+app.use(errorHandler)
+
+module.exports = app;
