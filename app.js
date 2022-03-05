5e14729e3af6557fb291f39b5e2b60619480e1fb
harshadbirajdar <hbirajdar1@gmail.com>
2022-03-05 16:53:28 +0530

    cors added

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { errorHandler, notFound } = require('./middleware/errorMiddleware')
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("tiny"));
@@ -16,7 +18,7 @@ const user = require("./route/user");
 // router middleware
 app.use("/api/v1", user);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
